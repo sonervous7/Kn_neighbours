e6c2362da211076b6d906c6e382739fdd12d7871
sonervous7 <jakub.buszynski@gmail.com>
2024-03-19 16:10:06 +0100

    Put condition "string.equals" before while loop for not repeating it.

--- src/DataReader.java
+++ src/DataReader.java
@@ -18,10 +18,11 @@ public class DataReader {
             scanner.nextLine();
         }
 
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] parts = line.split(";");
-            if (filepath.equals("test_data_cancer.txt") || filepath.equals("train_data_cancer.txt")) {
+
+        if (filepath.equals("test_data_cancer.txt") || filepath.equals("train_data_cancer.txt")) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(";");
                 double[] features = new double[parts.length - 2];
                 for (int i = 0; i < parts.length - 2; i++) {
                     try {
@@ -31,7 +32,11 @@ public class DataReader {
                     }
                 }
                 featuresList.add(features);
-            } else {
+            }
+        } else {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(";");
                 double[] features = new double[parts.length - 1]; // Ostatni element to etykieta, więc jest pomijany
                 for (int i = 0; i < parts.length - 1; i++) {
                     try {
@@ -42,12 +47,11 @@ public class DataReader {
                 }
                 featuresList.add(features);
             }
-
-
         }
         scanner.close();
         return featuresList;
     }
+
 
     /*
     Notatka: Kolekcje z pakietu java.util takie jak np. ArrayList<> nie mogą przechowywać typów prymitywnych jako elementów w kolekcjach
@@ -55,46 +59,45 @@ public class DataReader {
     W poniższej metodzie korzystam z opakowanego typu Integer (wrapperClass)
      */
 
-    public static ArrayList<Integer> readLabels(String filepath) {
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(new File(filepath));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        ArrayList<Integer> labelList = new ArrayList<>();
-
-        if (scanner.hasNextLine()) {
-            scanner.nextLine();
-        }
-
-
-
-        if (filepath.equals("test_data_cancer.txt") || filepath.equals("train_data_cancer.txt")) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] parts = line.split(";");
-                try {
-                    Integer label = Integer.parseInt(parts[1]);
-                    labelList.add(label);
-                } catch (NumberFormatException e) {
-                    System.err.println("Błąd podczas konwersji na liczbe INT (plik Cancer).");
-                }
-            }
-        } else {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] parts = line.split(";");
-                try {
-                    Integer label = Integer.parseInt(parts[parts.length-1]);
-                    labelList.add(label);
-                } catch (NumberFormatException e) {
-                    System.err.println("Błąd podczas konwersji na liczbę INT (plik Iris) " + parts[parts.length - 1]);
-                }
-            }
-        }
-        scanner.close();
-        return labelList;
+public static ArrayList<Integer> readLabels(String filepath) {
+    Scanner scanner = null;
+    try {
+        scanner = new Scanner(new File(filepath));
+    } catch (FileNotFoundException e) {
+        throw new RuntimeException(e);
     }
+    ArrayList<Integer> labelList = new ArrayList<>();
+
+    if (scanner.hasNextLine()) {
+        scanner.nextLine();
+    }
+
+
+    if (filepath.equals("test_data_cancer.txt") || filepath.equals("train_data_cancer.txt")) {
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(";");
+            try {
+                Integer label = Integer.parseInt(parts[1]);
+                labelList.add(label);
+            } catch (NumberFormatException e) {
+                System.err.println("Błąd podczas konwersji na liczbe INT (plik Cancer).");
+            }
+        }
+    } else {
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(";");
+            try {
+                Integer label = Integer.parseInt(parts[parts.length - 1]);
+                labelList.add(label);
+            } catch (NumberFormatException e) {
+                System.err.println("Błąd podczas konwersji na liczbę INT (plik Iris) " + parts[parts.length - 1]);
+            }
+        }
+    }
+    scanner.close();
+    return labelList;
 }
+    }
 
