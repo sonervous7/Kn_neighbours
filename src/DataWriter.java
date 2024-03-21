426ff235a3414f0316aaac3e6664a41c9c938bec
sonervous7 <jakub.buszynski@gmail.com>
2024-03-21 20:34:01 +0100

    New Writer Class.

---
+++ src/DataWriter.java
@@ -0,0 +1,24 @@
+import javax.imageio.IIOException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+Użycie FileWriter, ponieważ FileWriter automatycznie zarządza zasobami, w tym przypadku plikiem. Oznacza to, że automatycznie zamknie plik.
+ */
+public class DataWriter {
+
+    public static void clearFile(String filePath) {
+        try (FileWriter writer = new FileWriter(filePath, false)) {
+            // Samo otwarcie pliku w trybie nadpisywania, aby go wyczyścić
+        } catch (IOException e) {
+            System.err.println("Nie udało się wyczyścić pliku " + e.getMessage());
+        }
+    }
+    public static void saveResults(String filePath, String data) {
+        try (FileWriter writer = new FileWriter(filePath, true)) { // Parametr 'true' oznacza, że dane będa zapisywane do pliku
+            writer.write(data + "\n");
+        } catch (IOException e) {
+            System.err.println("Wystąpił błąd przy zapisie do pliku " + e.getMessage());
+        }
+    }
+}
